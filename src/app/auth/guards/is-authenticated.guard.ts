612c2746c982f66e6448ec364e81020ee18f17b8
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-23 10:07:37 +0200

    feat(auth): :sparkles: add localStorage and check token verification

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,7 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-  console.log('isAuthenticatedGuard is called');
-  console.log( {route, state})
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  console.log(authService.authStatus());
+  if(authService.authStatus() === AuthStatus.authenticated  ) {
+    return true;
+  }
+
+  const url = state.url;
+  localStorage.setItem('url', url);
+  router.navigate(['/auth/login']);
+
+  return false;
 };
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -39,4 +39,31 @@ export class AuthService {
       })
     )
   }
+
+  checkAuthStatus():Observable<boolean>{
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem("token");
+
+    if(!token) return of(false);
+
+    const headers = new HttpHeaders()
+    .set("Authorization",`Bearer ${token}`);
+
+    return this.httpClient.get<CheckTokenResponse>(url,{headers}).pipe(
+      map(({user,token})=>{
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token',token);
+        return true;
+      }),
+      catchError(()=>{
+        this._currentUser.set(null);
+        this._authStatus.set(AuthStatus.notAuthenticated);
+        return of(false);
+      })
+    );
+
+    return of(true);
+  }
+
 }
